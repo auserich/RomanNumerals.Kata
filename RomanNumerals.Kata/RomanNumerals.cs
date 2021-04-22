ca19c6e5f5f805b8b84380bd2882bd5f4baac213
Austin <a>
2021-04-21 20:57:00 -0700

    Added ReduceValue method to ConvertValue method so that test, ConvertValue_FourToIV now passes.

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -11,6 +11,7 @@ namespace RomanNumerals.Kata
             for (int i = 0; i < input; i++)
             {
                 output = output.Insert(output.Length, "I");
+                output = ReduceValue(output);
             }
 
             return output;
