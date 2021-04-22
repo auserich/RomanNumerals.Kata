0997090fc488e02506980cae65ecd6049d2ad4a7
Austin <a>
2021-04-21 19:41:46 -0700

    Added CheckNegative method added to RomanNumerals class to aid in negative value unit test.

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -1,10 +1,22 @@
-﻿namespace RomanNumerals.Kata
+﻿using System;
+
+namespace RomanNumerals.Kata
 {
     public class RomanNumerals
     {
         public static string ConvertValue(int input)
         {
             return "I";
+        }
+
+        public static int CheckNegative(int input)
+        {
+            if (input < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return input;
         }
     }
 }
