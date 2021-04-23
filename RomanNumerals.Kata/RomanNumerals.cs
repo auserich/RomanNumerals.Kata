a142a032a423c38af7903d934c86258d71251695
Austin <a>
2021-04-22 20:13:43 -0700

    Added logic to ConvertValue to allow conversion of values that include 50 and L.

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -13,7 +13,13 @@ namespace RomanNumerals.Kata
             while (input > 0)
             {
                 // Series of checks to find highest denominator of Roman numeral to match with input
-                if (input / 40 > 0)
+                if (input / 50 > 0)
+                {
+                    count = input / 50;
+                    output = BuildRomanNumeral(output, count, "L");
+                    input -= (50 * count);
+                }
+                else if (input / 40 > 0)
                 {
                     count = 1;
                     output = BuildRomanNumeral(output, count, "IL");
