f7b1a99f566837378a7cb49f6d9851d3c8530926
Austin <a>
2021-04-22 19:12:25 -0700

    Changed logic of ConvertValue to take on functionality of ReduceValue and added the method, BuildRomanNumeral, to be used to add values to an output string.

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -7,11 +7,34 @@ namespace RomanNumerals.Kata
         public static string ConvertValue(int input)
         {
             string output = "";
+            int count;
 
-            for (int i = 0; i < input; i++)
+            if (input / 5 > 0)
             {
-                output = output.Insert(output.Length, "I");
-                output = ReduceValue(output);
+                count = input / 5;
+                output = BuildRomanNumeral(output, count, "V");
+            }
+            else if (input / 4 > 0)
+            {
+                count = 1;
+                output = BuildRomanNumeral(output, count, "IV");
+            }
+            else if (input / 1 > 0)
+            {
+                count = input;
+                output = BuildRomanNumeral(output, count, "I");
+            }
+
+            return output;
+        }
+
+        public static string BuildRomanNumeral(string input, int count, string roman)
+        {
+            string output = input;
+
+            for (int i = 0; i < count; i++)
+            {
+                output = output.Insert(output.Length, roman);
             }
 
             return output;
