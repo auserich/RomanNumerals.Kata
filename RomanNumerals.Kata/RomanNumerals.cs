c6429081b5671faeeae262178ffe0c8cc344ebe9
Austin <a>
2021-04-22 19:20:29 -0700

    Added logic to ConvertValue method to allow conversion from 10 to X.

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -4,12 +4,19 @@ namespace RomanNumerals.Kata
 {
     public class RomanNumerals
     {
+        // Method that takes an integer value that builds a string of Roman numerals from the value of the input
         public static string ConvertValue(int input)
         {
             string output = "";
             int count;
 
-            if (input / 5 > 0)
+            // Series of checks to find highest denominator of Roman numeral to match with input
+            if (input / 10 > 0)
+            {
+                count = input / 10;
+                output = BuildRomanNumeral(output, count, "X");
+            }
+            else if (input / 5 > 0)
             {
                 count = input / 5;
                 output = BuildRomanNumeral(output, count, "V");
@@ -28,6 +35,8 @@ namespace RomanNumerals.Kata
             return output;
         }
 
+        // Method that takes an input string, an integer to use as a counter, and a string to represent the Roman
+        // numeral to append to the input string.
         public static string BuildRomanNumeral(string input, int count, string roman)
         {
             string output = input;
