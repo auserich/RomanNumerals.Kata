afe149bdcd09f952051eaaea4262096a744670f6
Austin <a>
2021-04-22 21:21:56 -0700

    Refactored ConvertValue so that the if-else logic is contained within two helper methods: GetRomanNumeral and GetDecimalValue so that there is less repeated logic within the main while-loop of ConvertValue.

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -9,91 +9,79 @@ namespace RomanNumerals.Kata
         {
             string output = "";
             int count;
+            int denominator;
+            string roman;
 
             while (input > 0)
             {
-                if (input / 1000 > 0)
-                {
-                    count = input / 1000;
-                    output = BuildRomanNumeral(output, count, "M");
-                    input -= (1000 * count);
-                }
-                // Series of checks to find highest denominator of Roman numeral to match with input
-                else if (input / 900 > 0)
-                {
-                    count = 1;
-                    output = BuildRomanNumeral(output, count, "CM");
-                    input -= 900;
-                }
-                else if (input / 500 > 0)
-                {
-                    count = input / 500;
-                    output = BuildRomanNumeral(output, count, "D");
-                    input -= 500;
-                }
-                else if (input / 400 > 0)
-                {
-                    count = 1;
-                    output = BuildRomanNumeral(output, count, "CD");
-                    input -= 400;
-                }
-                else if (input / 100 > 0)
-                {
-                    count = input / 100;
-                    output = BuildRomanNumeral(output, count, "C");
-                    input -= (100 * count);
-                }
-                else if (input / 90 > 0)
-                {
-                    count = 1;
-                    output = BuildRomanNumeral(output, count, "XC");
-                    input -= 90;
-                }
-                else if (input / 50 > 0)
-                {
-                    count = input / 50;
-                    output = BuildRomanNumeral(output, count, "L");
-                    input -= (50 * count);
-                }
-                else if (input / 40 > 0)
-                {
-                    count = 1;
-                    output = BuildRomanNumeral(output, count, "XL");
-                    input -= 40;
-                }
-                else if (input / 10 > 0)
-                {
-                    count = input / 10;
-                    output = BuildRomanNumeral(output, count, "X");
-                    input -= (10 * count);
-                }
-                else if (input / 9 > 0)
-                {
-                    count = 1;
-                    output = BuildRomanNumeral(output, count, "IX");
-                    input -= 9;
-                }
-                else if (input / 5 > 0)
-                {
-                    count = input / 5;
-                    output = BuildRomanNumeral(output, count, "V");
-                    input -= (5 * count);
-                }
-                else if (input / 4 > 0)
-                {
-                    count = 1;
-                    output = BuildRomanNumeral(output, count, "IV");
-                    input -= 4;
-                }
-                else if (input / 1 > 0)
-                {
-                    count = input;
-                    output = BuildRomanNumeral(output, count, "I");
-                    input -= (1 * count);
-                }
+                roman = GetRomanNumeral(input);
+                denominator = GetDecimalValue(roman);
+                count = input / denominator;
+                output = BuildRomanNumeral(output, count, roman);
+                input -= (denominator * count);
             }
 
             return output;
+        }
+
+        public static string GetRomanNumeral(int input)
+        {
+            if (input / 1000 > 0)
+                return "M";
+            else if (input / 900 > 0)
+                return "CM";
+            else if (input / 500 > 0)
+                return "D";
+            else if (input / 400 > 0)
+                return "CD";
+            else if (input / 100 > 0)
+                return "C";
+            else if (input / 90 > 0)
+                return "XC";
+            else if (input / 50 > 0)
+                return "L";
+            else if (input / 40 > 0)
+                return "XL";
+            else if (input / 10 > 0)
+                return "X";
+            else if (input / 9 > 0)
+                return "IX";
+            else if (input / 5 > 0)
+                return "V";
+            else if (input / 4 > 0)
+                return "IV";
+
+            return "I";
+        }
+
+        public static int GetDecimalValue(string input)
+        {
+            if (input == "M")
+                return 1000;
+            else if (input == "CM")
+                return 900;
+            else if (input == "D")
+                return 500;
+            else if (input == "CD")
+                return 400;
+            else if (input == "C")
+                return 100;
+            else if (input == "XC")
+                return 90;
+            else if (input == "L")
+                return 50;
+            else if (input == "XL")
+                return 40;
+            else if (input == "X")
+                return 10;
+            else if (input == "IX")
+                return 9;
+            else if (input == "V")
+                return 5;
+            else if (input == "IV")
+                return 4;
+
+            return 1;
         }
 
         // Method that takes an input string, an integer to use as a counter, and a string to represent the Roman
